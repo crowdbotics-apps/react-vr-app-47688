dafbf956df0530efb30402b885dc2217f2857a5c
Crowdbotics <team@crowdbotics.com>
2024-04-24 04:43:51 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "react-vr-app-47688",
+  url: "https://react-vr-app-47688.botics.co",
+  api: "https://react-vr-app-47688.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
